5a8bc3d4e49f06ca52b1ed6ffecb20b4b081c097
Vitaliy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-12-10 02:40:01 +0100

    feat(ui): add Spinner UI component

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,8 +90,13 @@ module.exports = {
           from: { height: "var(--radix-collapsible-content-height)" },
           to: { height: 0 },
         },
+        spin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
       },
       animation: {
+        spin: "spin 1s linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "collapsible-down": "collapsible-down 0.2s ease-in-out",
