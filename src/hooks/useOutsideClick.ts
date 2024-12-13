b5439581e55a11ecee7c1c8dfcc839060ccc4889
Vitaliy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-12-13 12:21:30 +0100

    feat(hooks): implement useOutsideClick hook

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,41 @@
+import { type Ref, useTemplateRef, onMounted, onUnmounted } from "vue";
+
+interface UseOutsideClickProps<T> {
+  templateRefName: string;
+  shown: Ref<T>;
+  closeHandler?: () => void;
+}
+
+export const useOutsideClick = <T>({
+  templateRefName,
+  shown,
+  closeHandler,
+}: UseOutsideClickProps<T>) => {
+  const elRef = useTemplateRef<HTMLElement>(templateRefName);
+
+  const defaultCloseHandler = () => {
+    if (typeof shown.value === "boolean") {
+      shown.value = false as T;
+    }
+  };
+
+  const handleOutsideClick = (event: Event) => {
+    if (
+      elRef.value &&
+      !elRef.value.contains(event.target as Node) &&
+      shown.value
+    ) {
+      (closeHandler ?? defaultCloseHandler)();
+    }
+  };
+
+  onMounted(() => document.addEventListener("mousedown", handleOutsideClick));
+
+  onUnmounted(() =>
+    document.removeEventListener("mousedown", handleOutsideClick)
+  );
+
+  return {
+    elRef,
+  };
+};
