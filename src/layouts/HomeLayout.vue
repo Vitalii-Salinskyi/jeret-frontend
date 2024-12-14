da2b8cd84a76320cc37ca8fdf9a7398508744e97
Vitaliy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-12-14 17:48:06 +0100

    feat(validation): add profile validation logic

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,13 +1,71 @@
 <script setup lang="ts">
+import { onBeforeMount, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+import { useSessionStore } from "@/stores/sessionStore";
+import { useToast } from "@/components/ui/toast";
+
 import Sidebar from "@/components/shared/Sidebar/Sidebar.vue";
+import Spinner from "@/components/ui/Spinner.vue";
+
+import { getProfile } from "@/api/users";
+import Header from "@/components/shared/Header.vue";
+import { headerVariants } from "@/constants/nav";
+
+const sessionStore = useSessionStore();
+const { toast } = useToast();
+const router = useRouter();
+const route = useRoute();
+
+const isLoading = ref<boolean>(true);
+
+onBeforeMount(async () => {
+  try {
+    let user = sessionStore.user;
+
+    if (!user) {
+      const { status, data } = await getProfile();
+
+      if (status === "failure") {
+        toast({
+          title: "Session error, redirecting to login",
+          variant: "destructive",
+        });
+        router.push("/auth");
+        return;
+      }
+
+      sessionStore.updateUser(data);
+      user = data;
+    }
+
+    if (!user.profile_completed) {
+      router.push("/auth");
+    }
+  } finally {
+    isLoading.value = false;
+  }
+});
 </script>
 
 <template>
-  <main class="flex">
+  <Spinner
+    v-if="isLoading"
+    size-class="size-14"
+    inner-class="text-main-purple-200"
+  />
+
+  <main class="flex" v-else>
     <Sidebar />
-    <header></header>
-    <div class="bg-[#FAFAFA] flex-1">
-      <RouterView />
+    <div class="flex flex-col flex-1">
+      <Header
+        v-if="route.name !== 'Dashboard'"
+        v-bind="headerVariants[route.name as string]"
+        :section-name="route.name as string"
+      />
+      <div class="bg-[#FAFAFA] flex-1">
+        <RouterView />
+      </div>
     </div>
   </main>
 </template>
