6a3a00c64c7ff8e5382e42a9825be42b8632eb46
Vitaliy <148259238+Vitaliy-Salinskiy@users.noreply.github.com>
2024-12-17 23:45:26 +0100

    feat(ui): add Carousel UI component

---
+++ src/components/ui/carousel/CarouselNext.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { WithClassAsProps } from "./interface";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-vue-next";
+import { useCarousel } from "./useCarousel";
+
+const props = withDefaults(
+  defineProps<WithClassAsProps & { isAbsolute?: boolean }>(),
+  {
+    isAbsolute: true,
+  }
+);
+
+const { orientation, canScrollNext, scrollNext } = useCarousel();
+</script>
+
+<template>
+  <Button
+    :disabled="!canScrollNext"
+    :class="
+      cn(
+        'touch-manipulation group h-8 w-8 rounded-full p-0',
+        isAbsolute
+          ? orientation === 'horizontal'
+            ? 'absolute -right-12 top-1/2 -translate-y-1/2'
+            : 'absolute -bottom-12 left-1/2 -translate-x-1/2 rotate-90'
+          : '',
+        props.class
+      )
+    "
+    variant="outline"
+    @click="scrollNext"
+  >
+    <div class="group-disabled:text-[#54577A]">
+      <slot>
+        <ArrowRight class="h-4 w-4 text-current" />
+        <span class="sr-only">Previous Slide</span>
+      </slot>
+    </div>
+  </Button>
+</template>
